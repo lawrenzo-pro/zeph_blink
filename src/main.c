8b905de790fde334b955f44ca678a4015dfb7c4d
Lawrence Kimani <larrykimani01@gmail.com>
2026-07-03 10:55:52 +0300

    Threads!!!

--- src/main.c
+++ src/main.c
@@ -1,12 +1,16 @@
-#include "zephyr/sys/printk.h"
-#include "zephyr/sys/util_macro.h"
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 #define DURATION 1000
+
 #define LED_NODE0 DT_ALIAS(led0)
 #define LED_NODE1 DT_ALIAS(led1)
 #define INT_LED DT_ALIAS(led2)
 #define BTN_NODE DT_ALIAS(btn0)
+#define STACK_SIZE 500
+K_THREAD_STACK_DEFINE(my_stack,STACK_SIZE)
+K_THREAD_STACK_DEFINE(my_second_stack, STACK_SIZE);
+struct k_thread my_thread;
+struct k_thread my_thread2;
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE0, gpios);
 static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED_NODE1, gpios);
 static const struct gpio_dt_spec btn = GPIO_DT_SPEC_GET(BTN_NODE, gpios);
@@ -21,47 +25,56 @@ void btn_handler(const struct device *port, struct gpio_callback *cb,gpio_port_p
     }
  //do sth later!!!
 }
-int main(void) {
-    int ret;
-    bool led_state = true;
+void init(){
+      int ret;
     if(!(gpio_is_ready_dt(&led) && gpio_is_ready_dt(&led1) && gpio_is_ready_dt(&btn)) && ! gpio_is_ready_dt(&int_led)){
-        return 0;
+        return;
     }
     ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
     if(ret < 0){
-        return 0;
+        return;
     }
     ret = gpio_pin_configure_dt(&int_led, GPIO_OUTPUT_ACTIVE);
     if(ret < 0){
-        return 0;
+        return;
     }
     ret = gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
     if(ret < 0){
-        return 0;
+        return;
     }
     ret = gpio_pin_configure_dt(&btn, GPIO_INPUT);
     if(ret < 0){
-        return 0;
+        return;
     }
     ret =gpio_pin_interrupt_configure_dt(&btn, GPIO_INT_EDGE_TO_ACTIVE);
     if(ret < 0){
-        return 0;
+        return;
     }
     gpio_init_callback(&callback_data, btn_handler, BIT(btn.pin));
     gpio_add_callback(btn.port, &callback_data);
-
+}
+void blink_func(void *p1,void *p2,void *p3){
     while(1){
-        ret = gpio_pin_toggle_dt(&led);
-        if(ret < 0){
-            return 0;
-        }
-        ret = gpio_pin_toggle_dt(&led1);
-        if(ret < 0){
-            return 0;
-        }
-        led_state = !led_state;
-        printf("led state: %d \n", led_state);
-        k_msleep(DURATION);
+        gpio_pin_toggle_dt(&led);
+        k_msleep(500);
+    } 
+}
+void blink_func_two(void *p1,void *p2,void *p3){
+    while(1){
+        gpio_pin_toggle_dt(&led1);
+        k_msleep(450);
+    }
+}
+int main(void) {
+    init();
+    k_tid_t blink_thread= k_thread_create(&my_thread,my_stack,
+        K_THREAD_STACK_SIZEOF(my_stack),
+    blink_func,NULL,NULL,NULL, 5,0,K_NO_WAIT);
+    k_tid_t blink_thread2 = k_thread_create(&my_thread2,my_second_stack,K_THREAD_STACK_SIZEOF(my_second_stack), 
+    blink_func_two, NULL, NULL, NULL, 5, 0, K_NO_WAIT);
+    while(1){
+       // no ret
+       k_msleep(1000);
     }
     return 0;
 }
